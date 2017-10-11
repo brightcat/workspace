0d0c3fa902585efa23ea4e86eb03f5abaefaf629
Anton van Eck <anton@brightcat.co.za>
2017-10-11 18:51:13 +0200

    format update

--- src/main/java/za/co/brightcat/tools/workspace/ParallelTask.java
+++ src/main/java/za/co/brightcat/tools/workspace/ParallelTask.java
@@ -3,10 +3,11 @@ package za.co.brightcat.tools.workspace;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ParallelTask implements Task{
-    private List<Task> taskList = new ArrayList<>();
+public class ParallelTask implements Task {
+    private final List<Task> taskList;
     
     public ParallelTask(Task... tasks) {
+        this.taskList = new ArrayList<>();
         for (Task task : tasks) {
             add(task);
         }
